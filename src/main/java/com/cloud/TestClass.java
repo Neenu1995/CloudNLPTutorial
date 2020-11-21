b49844fb16c0386fd03a191d36afb040938d75fc
neenushaji <neenushaji1995@gmail.com>
2020-11-20 22:34:51 -0500

    Cloud Natural Language Using client libraries tutorial

---
+++ src/main/java/com/cloud/TestClass.java
@@ -0,0 +1,51 @@
+package com.cloud;
+
+import com.google.api.gax.paging.Page;
+import com.google.cloud.language.v1.AnalyzeSentimentResponse;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+import java.io.IOException;
+
+public class TestClass {
+    public static void main(String... args) throws Exception {
+        // Instantiates a client
+        // Instantiate the Language client com.google.cloud.language.v1.LanguageServiceClient
+        String text = "I am sad";
+        authImplicit();
+        //  System.out.println(analyzeSentimentHere(text));
+    }
+
+
+    public static Sentiment analyzeSentimentHere(String text) throws IOException {
+        try (LanguageServiceClient language = LanguageServiceClient.create()) {
+            Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+            AnalyzeSentimentResponse response = language.analyzeSentiment(doc);
+            Sentiment sentiment = response.getDocumentSentiment();
+            if (sentiment == null) {
+                System.out.println("No sentiment found");
+            } else {
+                System.out.printf("Sentiment magnitude: %.3f\n", sentiment.getMagnitude());
+                System.out.printf("Sentiment score: %.3f\n", sentiment.getScore());
+            }
+            return sentiment;
+        }
+    }
+
+    static void authImplicit() {
+        // If you don't specify credentials when constructing the client, the client library will
+        // look for credentials via the environment variable GOOGLE_APPLICATION_CREDENTIALS.
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+
+        System.out.println("Buckets:");
+        Page<Bucket> buckets = storage.list();
+        for (Bucket bucket : buckets.iterateAll()) {
+            System.out.println(bucket.toString());
+        }
+    }
+}
